fb83349c8a64d30d2ab55a8c5cf247daf498003d
Elena <elenalapalmera17@gmail.com>
2025-12-01 09:36:36 +0100

    Programa completo hecho junto con el tester

--- src/main/java/daw/Paqueteria.java
+++ src/main/java/daw/Paqueteria.java
@@ -13,32 +13,41 @@ public class Paqueteria {
         double pesoPaquete = 0;
         char zonaDestino = ' ';
     
-
+        // Bucle que valida que el usuario introduzca correctamente los kilos del paquete
         do {
             try {
-                System.out.println("----------------------------------");
+                System.out.println("------------------------------------");
                 System.out.print("Introduce el peso del paquete en kg: ");
                 pesoPaquete = teclado.nextDouble();
-                System.out.println("----------------------------------");
                 repetir = false;
 
             } catch (InputMismatchException ime) {
+                System.out.println("-----------------------------------------------------------------------------------------------");
                 System.out.println("Error: Debes introducir un número válido para el peso. Recuerda no utilizar letras ni símbolos.");
                 teclado.nextLine(); // Limpiar el buffer del scanner
                 repetir = true;
             }
         } while (repetir);
 
+        teclado.nextLine(); // Limpiar el buffer del scanner antes de leer un char
+
+        // Bucle que valida que el usuario introduzca correctamente la zona de destino
         do {
             try {
-                System.out.println("--------------------------------------");
+                System.out.println("----------------------------------------");
                 System.out.print("Introduce la zona de destino (A, B o C): ");
                 zonaDestino = teclado.next().toUpperCase().charAt(0);
-                System.out.println("--------------------------------------");
+                if (zonaDestino != 'A' && zonaDestino != 'B' && zonaDestino != 'C') {
+                    System.out.println("-------------------------------------------------------");
+                    System.out.println("Error -2: Zona de destino no válida. Debe ser A, B o C.");
+                    repetir = true;
+                    continue;
+                }
                 repetir = false;
                 
             } catch (InputMismatchException ime) {
-                System.out.println("Error: Debes introducir una letra válida. No utilices números ni símbolos.");
+                System.out.println("-----------------------------------------------------------------------------");
+                System.out.println("Error -1: Debes introducir una letra válida. No utilices números ni símbolos.");
                 teclado.nextLine(); // Limpiar el buffer del scanner
                 repetir = true;
             }
@@ -46,8 +55,67 @@ public class Paqueteria {
 
         System.out.println("El coste de envío es: " + calcularTarifaEnvio(pesoPaquete, zonaDestino) + " euros.");
 
+        teclado.nextLine(); // Limpiar el buffer del scanner antes de leer una cadena
+
+        // Bucle que valida que el usuario introduzca correctamente el identificador del paquete
+        String idPaquete = "";
+        do {
+            System.out.println("---------------------------------------------------------------");
+            System.out.print("Introduce el identificador del paquete (4 dígitos entre 1 y 9): ");
+            idPaquete = teclado.nextLine();
+            System.out.println("-------------------------------------");   
+            if (validarIdentificador(idPaquete)) {
+                System.out.println("El identificador del paquete es válido.");
+                repetir = false;
+            } else {
+                System.out.println("Error: El identificador del paquete no es válido. Debe tener 4 dígitos entre 1 y 9.");
+                repetir = true;
+            }
+        } while (repetir);
+
+        // Bucle que valida que el usuario introduzca correctamente el número de paquetes
+        int totalPaquetes = 0;
+        do {
+            try {
+                System.out.println("-------------------------------------------------");
+                System.out.print("Introduce el número total de paquetes a repartir: ");
+                totalPaquetes = teclado.nextInt();
+                repetir = false;
+            } catch (InputMismatchException ime) {
+                System.out.println("-------------------------------------------------------------------");
+                System.out.println("Error: Debes introducir un número válido para el total de paquetes.");
+                teclado.nextLine(); // Limpiar el buffer del scanner
+                repetir = true;
+            }
+        } while (repetir);
+
+        // Bucle que valida que el usuario introduzca correctamente el número de camiones
+        int numeroCamiones = 0;
+        do {
+            try {
+                System.out.println("-------------------------------------------------------");
+                System.out.print("Introduce el número total de camiones que van a repartir: ");
+                numeroCamiones = teclado.nextInt();
+                if (numeroCamiones < 0) {
+                    System.out.println("------------------------------------------------");
+                    System.out.println("Error: Introduce un número igual o mayor a cero.");
+                    repetir = true; 
+                } 
+                repetir = false;
+
+            } catch (InputMismatchException ime) {
+                System.out.println("-------------------------------------------------------------------------------------------");
+                System.out.println("Error: Debes introducir un número válido sin letras ni decimales para el total de camiones.");
+                teclado.nextLine(); // Limpiar el buffer del scanner
+                repetir = true;
+            }
+        } while (repetir);
+
+        System.out.println("El número de paquetes que cada camión llevará serán: " + repartirCarga(totalPaquetes, numeroCamiones));
+
     }
     
+    // Función que calcula la tarifa base del envío en función de la zona de envío y del peso del paquete
     public static double calcularTarifaEnvio(double peso, char zona) {
         double costeBase;
         double costeTotal = 0;
@@ -83,6 +151,40 @@ public class Paqueteria {
         
 
         return costeTotal;
+    }
+
+    // Función que verifica que el identificador del paquete tiene 4 caracteres y estos dígitos están entre 1 y 9
+    public static boolean validarIdentificador(String id){
+        boolean valido = true;
+
+        if (id.length() != 4){
+            return valido = false;
+        }
+
+        for (int i = 0; i < id.length(); i++) {
+            char caracter = id.charAt(i);
+            if (caracter < '1' || caracter > '9') {
+                return valido = false;
+            }
+        }
+
+        return valido;
+    }
+
+    // Función que reparte la carga de paquetes entre los camiones disponibles
+    // Si se lanza la excepción, devolver 0
+    // División entera entre el total de paquetes y el número de camiones
+
+    public static int repartirCarga(int totalPaquetes, int camiones) {
+        int cargaPorCamion = 0;
+        try {
+            cargaPorCamion = totalPaquetes / camiones;
+        } catch (ArithmeticException ae) {
+            System.out.println("Error: El número de camiones no puede ser cero.");
+            return 0;
+        }
+        
+        return cargaPorCamion;
     }
 
 }
---
+++ src/test/Java/endes/PaqueteriaTest.java
@@ -0,0 +1,60 @@
+package endes;
+import daw.Paqueteria;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PaqueteriaTest {
+    @Test 
+    public void testCalcularTarizaZonaA() {
+        int resultado = Paqueteria.calcularTarifaEnvio(3, "A");
+        assertEquals(10, resultado, "La tarifa para zona A y peso 3 debería ser 10.");
+    }
+
+    @Test 
+    public void testCalcularTarifaZonaBConSuplemento() {
+        int resultado = Paqueteria.calcularTarifaEnvio(6, "B");
+        assertEquals(20, resultado, "La tarifa para zona B y peso 6 debería ser 20.");
+    }
+
+    @Test 
+    public void testCalcularTarifaPesoInvalido() {
+        int resultado = Paqueteria.calcularTarifaEnvio(-5, "A");
+        assertEquals(-1, resultado, "El peso no debe ser negativo.");
+    }
+
+    @Test 
+    public void testValidarIdentificadorCorrecto() {
+        boolean resultado = Paqueteria.validarIdentificador("1234");
+        assertTrue(resultado, "El identificador debería ser válido.");
+    }
+
+    @Test 
+    public void testValidarIdentificadorIncorrectoLongitud() {
+        boolean resultado = Paqueteria.validarIdentificador("123");
+        assertFalse(resultado, "El identificador debe tener exactamente 4 dígitos.");
+    }
+
+    @Test 
+    public void testValidarIdentificadorIncorrectoLetra() {
+        boolean resultado = Paqueteria.validarIdentificador("12A4");
+        assertFalse(resultado, "El identificador debe contener solo dígitos entre 1 y 9.");
+    }
+
+    @Test 
+    public void testRepartirCargaCorrecta() {
+        int resultado = Paqueteria.repartirCarga(10, 2);
+        assertEquals(5, resultado, "Cada camión debería llevar 5 paquetes.");
+    }
+
+    @Test 
+    public void testRepartirCargaConResto() {
+        int resultado = Paqueteria.repartirCarga(10, 3);
+        assertEquals(3, resultado, "Cada camión debería llevar 3 paquetes.");
+    }
+
+    @Test 
+    public void testRepartirCargaErrorCero() {
+        int resultado = Paqueteria.repartirCarga(50, 0);
+        assertEquals(0, resultado, "El resultado debería ser 0 cuando el número de camiones es cero.");
+    }
+
+}
